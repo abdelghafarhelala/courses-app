ed973eeaf368799c2de56e8359d0a36e78687238
abdelghafar-macber <abdulghaffar@macber-eg.com>
2026-05-13 20:46:40 +0300

    Implement API enhancements: add error handling, pagination, and environment configuration

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
+require('dotenv').config();
 const express = require('express');
+var cors = require('cors');
 const coursesRouter = require('./routes/courses.route');
+const httpStatusText = require('./utils/httpStatusText');
+
 const app = express();
-const port = 4001;
+const port = process.env.PORT || 4001;
 const mongoose = require('mongoose');
 
 app.use(express.json());
-app.use('/api/courses',coursesRouter);
+app.use(cors());
 
-const uri = "mongodb+srv://abdo:iYsLuAHKYArke9D@cluster0.h8a3xmg.mongodb.net/courses?appName=Cluster0";
+app.use('/api/courses', coursesRouter);
+
+app.all("/{*splat}", (req, res) => {
+    res.status(404).json({ status: httpStatusText.FAIL, message: "Route not found", data: null });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.statusCode || 500).json({ status:err.status || httpStatusText.ERROR, 
+                                             message: err.message || "Internal Server Error", 
+                                             data: err.data });
+});
+
+const uri = process.env.MONGO_URL;
 mongoose.connect(uri).then(() => {
     console.log('Connected to MongoDB');
     app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+        console.log(`Example app listening on port ${port}`);
+    });
 }).catch((error) => {
     console.error('Error connecting to MongoDB:', error);
 });
--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,57 +1,60 @@
 
 const Course = require('../models/course.model');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
+const httpStatusText = require('../utils/httpStatusText');
+const asyncWrapper = require('../middelwares/asyncWrapper');
 
- const getAllCourses = async (req, res) => {
-    const courses = await Course.find();
-    res.json(courses);
-};
+const getAllCourses = asyncWrapper(async (req, res) => {
+    const query = req.query;
+    const limit = parseInt(query.limit) || 2;
+    const page = parseInt(query.page) || 1;
+    const skip = (page - 1) * limit;
+    const courses = await Course.find({}, { __v: 0 }).limit(limit).skip(skip);
+    res.json({ status: httpStatusText.SUCCESS, data: courses });
+});
 
-const createCourse = async (req,res)=>{
+const createCourse = asyncWrapper(async (req, res, next) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        console.log(validationResult(req).errors);
-        return res.status(400).json(errors.array());
+    if (!errors.isEmpty()) {
+        return next({ statusCode: 400, status: httpStatusText.FAIL, message: "Validation failed", data: errors.array() });
     }
     const newCourse = new Course(req.body);
-    const result =await newCourse.save();
-    if(!result){
-        return res.status(400).json({message:"Error creating course"});
+    const result = await newCourse.save();
+    if (!result) {
+        return next({ status: httpStatusText.FAIL, message: "Course creation failed", data: null });
     }
-    res.status(201).json(result);
-}
-const getCourse = async (req, res) => {
-   try {
-     const course = await Course.findById(req.params.id);
+    res.status(201).json({ status: httpStatusText.SUCCESS, message: "Course created successfully", data: result });
+});
+
+const getCourse = asyncWrapper(async (req, res) => {
+    const course = await Course.findById(req.params.id, { __v: 0 });
     if (!course) {
-        return res.status(404).json({ message: 'Course not found' });
+        return res.status(404).json({ status: httpStatusText.FAIL, message: "Course not found", data: null });
     }
-   return  res.json(course);
-   } catch (error) {
-    return res.status(400).json({ message: "Invalid course ID" });
-   }
-}
+    return res.json({ status: httpStatusText.SUCCESS, data: course });
+});
+
 const updateCourse = async (req, res) => {
-   try {
-     const course = await Course.findByIdAndUpdate(req.params.id, req.body);
-    if (!course) {
-        return res.status(404).json({ message: 'Course not found' });
+    try {
+        const course = await Course.findByIdAndUpdate(req.params.id, req.body);
+        if (!course) {
+            return res.status(404).json({ status: httpStatusText.FAIL, message: "Course not found", data: null });
+        }
+        return res.json({ status: httpStatusText.SUCCESS, data: null, message: "Course updated successfully" });
+    } catch (error) {
+        return res.status(400).json({ status: httpStatusText.ERROR, message: error.message, data: null });
     }
-    return res.json(course);
-   } catch (error) {
-   return res.status(400).json({ message: "Invalid course ID" });
-   }
 }
 const deleteCourse = async (req, res) => {
-   try {
-     const course = await Course.findByIdAndDelete(req.params.id);
-    if (!course) {
-        return res.status(404).json({ message: 'Course not found' });
+    try {
+        const course = await Course.findByIdAndDelete(req.params.id);
+        if (!course) {
+            return res.status(404).json({ status: httpStatusText.FAIL, message: "Course not found", data: null });
+        }
+        return res.json({ status: httpStatusText.SUCCESS, message: "Course deleted successfully", data: null });
+    } catch (error) {
+        return res.status(400).json({ status: httpStatusText.ERROR, message: error.message, data: null });
     }
-    return res.json({ message: 'Course deleted successfully' });
-   } catch (error) {
-    return res.status(400).json({ message: "Invalid course ID" });
-   }
 }
 
 module.exports = {
